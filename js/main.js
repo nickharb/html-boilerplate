cb7218d109d009db2628efd631b79852ffccd5dd
Nick Harbaugh <nickharb@nicks-mbp.lan>
2020-10-06 11:36:45 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+/**
+ * HTML Boilerplate
+ *
+ * Copyright 2020 Seven Mile Media
+ * Created by Seven Mile Media
+ * http://sevenmilemedia.com/
+ */
+
+(function($, _) {
+
+    // -------------------------------------------------------- //
+    // Variables
+    // -------------------------------------------------------- //
+
+    var msg = 'Hello world!';
+
+    // -------------------------------------------------------- //
+    // Initialize
+    // -------------------------------------------------------- //
+
+    function initialize() {
+        console.log(msg);
+    }
+
+    // -------------------------------------------------------- //
+    // Document ready
+    // -------------------------------------------------------- //
+
+    $(document).ready(function() {
+        initialize();
+    });
+
+
+})(jQuery, _);
